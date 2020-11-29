1dfa5b0cdc40fa041cde5c2ec182531f07693f7f
RyutoShimada <business.sub10110@gmail.com>
2020-11-29 15:09:58 +0900

    星に反応して星座が光るようにした 全て光ると、ゲームクリアのリザルトシーンに移行するようにした hierarchyを見やすくした

--- Assets/Kita/Script/Constellation1.cs
+++ Assets/Kita/Script/Constellation1.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class Constellation1 : MonoBehaviour
 {
     public GameObject[] StarPrefabs; //星のプレハブを管理する配列
-    int starIndex = 0;    
+    int starIndex = 0;
+
+    [SerializeField] GameObject loadSceneManagerObject = default;
+    LoadSceneManagerM loadSceneManager;
 
     void Start()
     {
-
+        loadSceneManager = loadSceneManagerObject.GetComponent<LoadSceneManagerM>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (starIndex == StarPrefabs.Length)
         {
-            Flg();
-            starIndex++;
+            loadSceneManager.LoadResultScene();
         }
-        
     }
 
     public void Flg()
     {
         // 出現する星のプレハブを配列から取得する
         StarPrefabs[starIndex].SetActive(false);
+        //Debug.Log("index:" + starIndex);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Star")
+        {
+            Flg();
+            starIndex++;
+        }
     }
 }

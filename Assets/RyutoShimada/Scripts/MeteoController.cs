f317f3df392e4ebff0256002f0a38258a4425efb
RyutoShimada <business.sub10110@gmail.com>
2020-11-29 12:43:38 +0900

    Generator完成

--- Assets/RyutoShimada/Scripts/MeteoController.cs
+++ Assets/RyutoShimada/Scripts/MeteoController.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class MeteoController : MonoBehaviour
 {
-    //[SerializeField] float m_meteoSpeed = 5f;
+    /// <summary>落下速度</summary>
+    [SerializeField] float m_speed = 5f;
+
+    /// <summary>落下速度をランダムにするかどうか</summary>
+    [SerializeField] bool m_randomSpeed = false;
+    /// <summary>ランダムの最低値</summary>
+    [SerializeField] float m_rangeMin = 1f;
+    /// <summary>ランダムの最高値</summary>
+    [SerializeField] float m_rangeMax = 3f;
+
+    Rigidbody2D m_rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_randomSpeed) //ランダムON
+        {
+            m_rb.AddForce(Vector2.down * Random.Range(m_rangeMin, m_rangeMax), ForceMode2D.Force);
+        }
+        else
+        {
+            m_rb.AddForce(Vector2.down * m_speed, ForceMode2D.Force);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
